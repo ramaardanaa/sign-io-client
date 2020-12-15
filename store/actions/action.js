31d0f7b5e65e7b5db91e8ef920f62c5b50ce2bb8
ramaardanaa <ramaardanaa@gmail.com>
2020-12-15 16:22:16 +0700

    Solve recording speech to text

--- store/actions/action.js
+++ store/actions/action.js
@@ -23,26 +23,22 @@ export function login(payload) {
   }
 }
 
-// export function login(payload) {
-//   console.log(payload)
-//   return(dispatch) => {
-//     axios({
-//       method:"post",
-//       url: "/users/login",
-//       data:{
-//         email: payload.email,
-//         password: payload.password
-//       }
-//     })
-//     .then(({data}) => {
-//       console.log(data)
-//       dispatch({
-//         type:"SET_TOKEN",
-//         payload: data.access_token
-//       })
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     })
-//   }
-// }
+export function register(payload) {
+  return(dispatch) => {
+    axios({
+      method:"post",
+      url: "/users/register",
+      data:{
+        name:payload.name,
+        email: payload.email,
+        password: payload.password
+      }
+    })
+    .then(({data}) => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+}
