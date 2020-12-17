be1dc682dc744619d517a0f52e8238aac26a0608
Nikolas Stefano <nz.stefano@gmail.com>
2020-12-17 13:54:04 +0700

    fix predictions bug

--- axios/axios.js
+++ axios/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const urlNiko = "http://192.168.2.61:3000";
+const urlRama = "http://192.168.1.143:3005";
+
 const instance = axios.create({
-  baseURL: "http://192.168.1.143:3005",
+  baseURL: urlNiko,
 });
 
 export default instance;
--- pages/SignToText.js
+++ pages/SignToText.js
@@ -15,6 +15,16 @@ import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
 import mime from "mime";
 import axios from "axios";
+import * as ImageManipulator from "expo-image-manipulator";
+import * as tf from "@tensorflow/tfjs";
+import {
+  fetch,
+  decodeJpeg,
+  bundleResourceIO,
+} from "@tensorflow/tfjs-react-native";
+
+const modelJson = require("../assets/models/model.json");
+const modelWeights = require("../assets/models/model.weights.bin");
 
 export default function SignToText({ navigation }) {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
@@ -26,21 +36,21 @@ export default function SignToText({ navigation }) {
   const screenRatio = height / width;
   const [isRatioSet, setIsRatioSet] = useState(false);
   const isFocused = useIsFocused();
-  // const [pictureSaved, setPictureSaved] = useState();
   const [result, setResult] = useState();
+  // const [iseng, setIseng] = useState();
 
-  let textureDims;
-  if (Platform.OS === "ios") {
-    textureDims = {
-      height: 1920,
-      width: 1080,
-    };
-  } else {
-    textureDims = {
-      height: 1200,
-      width: 1600,
-    };
-  }
+  // let textureDims;
+  // if (Platform.OS === "ios") {
+  //   textureDims = {
+  //     height: 1920,
+  //     width: 1080,
+  //   };
+  // } else {
+  //   textureDims = {
+  //     height: 1200,
+  //     width: 1600,
+  //   };
+  // }
 
   useEffect(() => {
     async function getCameraStatus() {
@@ -54,7 +64,9 @@ export default function SignToText({ navigation }) {
     let desiredRatio = "4:3";
     if (Platform.OS === "android") {
       const ratios = await camera.getSupportedRatiosAsync();
-      console.log(ratios)
+      // const size = await camera.getAvailablePictureSizesAsync();
+      // console.log("size", size);
+      console.log(ratios);
       let distances = {};
       let realRatios = {};
       let minDistance = null;
@@ -117,16 +129,28 @@ export default function SignToText({ navigation }) {
   const onPictureSaved = async (photo) => {
     const { uri } = photo;
     console.log(photo);
-    // setPictureSaved(photo.uri);
+    console.log(photo.uri);
+    // setIseng(uri);
+
     try {
+      await tf.setBackend("rn-webgl");
+      await tf.ready();
+      const manipResult = await ImageManipulator.manipulateAsync(
+        uri,
+        [{ resize: { width: 224, height: 224 } }],
+        { format: "png" }
+      );
+      console.log(manipResult);
       console.log("masuk");
       console.log("uri", uri);
-      const type = mime.getType(uri);
+      const type = mime.getType(manipResult.uri);
+      // const type = mime.getType(uri);
       const ext = mime.getExtension(type);
 
       let formData = new FormData();
       formData.append("file", {
-        uri: uri,
+        uri: manipResult.uri,
+        // uri: uri,
         type: type,
         name: `${Date.now()}.${ext}`,
       });
@@ -144,14 +168,34 @@ export default function SignToText({ navigation }) {
       );
       console.log("test");
       console.log("data", data);
-      const { predictions } = await axios.post(
-        "http://192.168.2.61:3000/sign",
-        {
-          url: data,
-        }
-      );
-      console.log(predictions);
-      // setResult(predictions);
+      const hasil = await axios.post("http://192.168.2.61:3000/sign", {
+        url: data.file,
+      });
+      console.log(hasil.data.predictions);
+      setResult(hasil.data.predictions);
+
+      // Use the bundleResorceIO IOHandler to load the model
+      // const model = await tf.loadLayersModel(
+      //   bundleResourceIO(modelJson, modelWeights)
+      // );
+
+      // Load an image from the web
+      // const uri2 = data.file;
+      // const uri2 = manipResult;
+      // const imageAssetPath = Image.resolveAssetSource(uri2);
+      // console.log("uri2", uri2);
+
+      // const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
+      // console.log("response", response);
+      // const imageData = await response.arrayBuffer();
+      // console.log("imageData", imageData);
+      // const imageTensor = decodeJpeg(uri);
+      // const imageTensor = decodeJpeg(manipResult);
+
+      // const prediction = (await model.predict(imageTensor))[0];
+      // console.log(prediction);
+      // Use prediction in app
+      // setResult(prediction);
     } catch (error) {
       console.log(error);
     }
@@ -160,37 +204,40 @@ export default function SignToText({ navigation }) {
   return (
     <View style={styles.container}>
       <Button
-            onPress={() => navigation.navigate("GroupRoom")}
-            color="#9F62FF"
-            style={{ width: 5, marginTop: 0, marginBottom: 10, marginLeft: 15 }}
-            labelStyle={{ fontSize: 50 }}
-            mode="text"
-            icon={require("../assets/x.png")}
-          />
-      <View style={{height:450}}>
-      {isFocused && (
-        <Camera
-          type={Camera.Constants.Type.back}
-          style={[styles.cameraPreview, { marginTop: imagePadding }]}
-          onCameraReady={setCameraReady}
-          ratio={'1:1'}
-          ref={(ref) => {
-            setCamera(ref);
-          }}
-          // cameraTextureHeight={textureDims.height}
-          // cameraTextureWidth={textureDims.width}
-          // resizeHeight={200}
-          // resizeWidth={152}
-          // resizeDepth={3}
-          // onReady={handleCameraStream}
-          // autorender={true}
-        >
-          
-        </Camera>
-      )}
+        onPress={() => navigation.navigate("GroupRoom")}
+        color="#9F62FF"
+        style={{ width: 5, marginTop: 0, marginBottom: 10, marginLeft: 15 }}
+        labelStyle={{ fontSize: 50 }}
+        mode="text"
+        icon={require("../assets/x.png")}
+      />
+      <View style={{ height: 450 }}>
+        {isFocused && (
+          <Camera
+            type={Camera.Constants.Type.back}
+            style={[styles.cameraPreview, { marginTop: imagePadding }]}
+            onCameraReady={setCameraReady}
+            ratio={"1:1"}
+            ref={(ref) => {
+              setCamera(ref);
+            }}
+            // cameraTextureHeight={textureDims.height}
+            // cameraTextureWidth={textureDims.width}
+            // resizeHeight={200}
+            // resizeWidth={152}
+            // resizeDepth={3}
+            // onReady={handleCameraStream}
+            // autorender={true}
+          ></Camera>
+        )}
       </View>
       <Button
-        style={{ borderRadius: 20,paddingVertical:10, marginTop:20,marginHorizontal:20 }}
+        style={{
+          borderRadius: 20,
+          paddingVertical: 10,
+          marginTop: 20,
+          marginHorizontal: 20,
+        }}
         mode="contained"
         onPress={() => {
           takePicture();
@@ -198,13 +245,17 @@ export default function SignToText({ navigation }) {
       >
         Translate
       </Button>
-      <Text
-        label="Translated Sign"
-        mode="outlined"
-        style={{ marginBottom: 20 }}
-      >
-        {result}
-      </Text>
+
+      {/* Mas rama edit yg ini  */}
+      {result && (
+        <Text
+          label="Translated Sign"
+          mode="outlined"
+          style={{ marginBottom: 20 }}
+        >
+          {result}
+        </Text>
+      )}
     </View>
   );
 }
