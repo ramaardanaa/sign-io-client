5158b5918ba85cc122476b5dac18a42384550fe9
ramaardanaa <ramaardanaa@gmail.com>
2020-12-17 09:04:09 +0700

    fix details group

--- pages/GroupDetail.js
+++ pages/GroupDetail.js
@@ -13,7 +13,7 @@ export default function GroupDetail({navigation,route}){
           <Button color='#834ea8' onPress={() => navigation.goBack()} style={{width:5,marginTop:25,marginBottom:5,marginLeft:15}} labelStyle={{fontSize:20}} mode='text' icon={require('../assets/back.png')}/>
         </View>
       <View style={{alignItems:'center',marginTop:0}}>
-        <Text style={{fontFamily:'Montserratlight', fontSize:25, marginTop:20}}>{room.name}</Text>
+        <Text style={{fontFamily:'Montserratlight', fontSize:25, marginTop:20}}>#{room.name}</Text>
         <Text style={{fontFamily:'Montserratlight', fontSize:15}}>ID: {room.code}</Text>
       </View>
       <View>
